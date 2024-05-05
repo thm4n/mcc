b6cfc4edfce5d2c901a8b8597672be0a6d42d65e
thm4n <t1amir054@gmail.com>
2024-05-05 23:44:17 +0300

    added lexer and logic. support basic C syntax, does not support strings of any kind.

--- src/Lexer.c
+++ src/Lexer.c
@@ -1,8 +1,12 @@
 #include "Lexer.h"
 
+#define TOK_SIZE 32
+
 int lexer(char* inputFilePath, vector* tokens) {
 	FILE* inFile = NULL;
-	char token[32] = {0};
+	char token[TOK_SIZE] = {0};
+	char* str = NULL;
+	int index = 0;
 	char ch = 0;
 
 	inFile = fopen(inputFilePath, "r");
@@ -11,8 +15,35 @@ int lexer(char* inputFilePath, vector* tokens) {
 		return _errcode_IO;
 	}
 
-	while(fread(&ch, 1, 1, inFile) == 1) {
-		
+	while(!feof(inFile) && index < TOK_SIZE) {
+		if(fread(&ch, 1, 1, inFile) != 1) {
+			dbglog("fread returned != 1: ch holds value %d", (int)ch);
+		}
+
+		if(isWS(ch)) {
+			if(index) {
+				str = allocateStr(token, 0);
+				vector_append(tokens, str);
+				memset(token, 0, TOK_SIZE);
+				index = 0;
+			}
+			else {
+				dbglog("tried to append to tokens vector empty token");
+			}
+		}
+		else if(isChar(ch) || isDigit(ch)) {
+			token[index++] = ch;
+		}
+		else if(isSpec(ch)) {
+			if(index) {
+				str = allocateStr(token, 0);
+				vector_append(tokens, str);
+				memset(token, 0, TOK_SIZE);
+				index = 0;
+			}
+			str = allocateStr(&ch, 1);
+			vector_append(tokens, str);
+		}
 	}
 
 	return _errcode_clean;
