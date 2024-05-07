bd396c1de033642998d6f3cdb366f630037c914e
thm4n <t1amir054@gmail.com>
2024-05-07 17:04:19 +0300

    made main accept multiple files, each file is lexed in handleFile, which is separate.

--- src/main.c
+++ src/main.c
@@ -1,19 +1,40 @@
 #include "Header.h"
 
+int handleFile(char* filePath);
+
 int main(int argc, char** argv) {
+	int res = 0;
+
 	initLogger(argc, argv);
 
-	if(argc != 2) {
+	if(argc <=  1) {
 		errlog(ERRCODE_USAGE_MSG);
 		return _errcode_usage;
 	}
 
+	for(int i = 1; i < argc; i++) {
+		if((res = handleFile(argv[i]))) {
+			dbglog("handleFile failed with errno: %d", res);
+			return res;
+		}
+	}
+
+	return 0;
+}
+
+int handleFile(char* filePath) {
 	vector* tokens = vector_ctor();
 
-	lexer(argv[1], tokens);
+	int res = lexer(filePath, tokens);
 
+	if(res != EXIT_SUCCESS) {
+		dbglog("lexer failed with errno: %d", res);
+		return res;
+	}
+
+	printf("------- %s -------\n", filePath);
 	for(int i = 0; i < tokens->_length; i++) {
-		dbglog("tok[%d]: '%s'", i + 1, tokens->_array[i]);
+		printf("tok[%d]: '%s'\n", i + 1, (char*)tokens->_array[i]);
 		if(tokens->_array[i])
 			free(tokens->_array[i]);
 	}
