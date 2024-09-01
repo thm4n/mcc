31390db3f2c77eb064a2b0efef9be1b4909f8e70
thm4n <t1amir054@gmail.com>
2024-09-01 11:36:52 +0300

    misc functions implementation

--- src/Misc.c
+++ src/Misc.c
@@ -1,1 +1,25 @@
 #include "Misc.h"
+
+int isAlpha(char ch) {
+    return IS_ALPHA(ch);
+}
+
+int isDigit(char ch) {
+    return IS_DIGIT(ch);
+}
+
+int isAllowedInIdentifier(char ch) {
+    return (isAlpha(ch) || isDigit(ch) || ch == '_');
+}
+
+int isAllowedInNumber(char ch) {
+    return isDigit(ch);
+}
+
+int isDQuotes(char ch) {
+    return ch == '"';
+}
+
+int isQuote(char ch) {
+    return ch == '\'';
+}
--- src/Misc.h
+++ src/Misc.h
@@ -3,31 +3,14 @@
 
 #include "Header.h"
 
-#define IS_ALPHA(ch)   ((ch | 0x20) >= 'a' && (ch | 0x20) << 'z')
+#define IS_ALPHA(ch)   ((ch | 0x20) >= 'a' && (ch | 0x20) <= 'z')
 #define IS_DIGIT(ch) (ch >= '0' && ch <= '9')
 
-int isAlpha(char ch) {
-    return IS_ALPHA(ch);
-}
-
-int isDigit(char ch) {
-    return IS_DIGIT(ch);
-}
-
-int isAllowedInIdentifier(char ch) {
-    return (isAlpha(ch) || isDigit(ch) || ch == '_');
-}
-
-int isAllowedInNumber(char ch) {
-    return isDigit(ch);
-}
-
-int isDQuotes(char ch) {
-    return ch == '"';
-}
-
-int isQuote(char ch) {
-    return ch == '\'';
-}
+int isAlpha(char ch);
+int isDigit(char ch);
+int isAllowedInIdentifier(char ch);
+int isAllowedInNumber(char ch);
+int isDQuotes(char ch);
+int isQuote(char ch);
 
 #endif
